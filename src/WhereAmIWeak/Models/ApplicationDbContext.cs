740284ad75d7917821fbdf1e9b0f08ddfb0eab04
Perry Taylor <pltaylor3@gmail.com>
2015-04-22 14:45:48 -0400

    Continue refactoring files

--- src/WhereAmIWeak/Models/ApplicationDbContext.cs
+++ src/WhereAmIWeak/Models/ApplicationDbContext.cs
@@ -1,17 +1,10 @@
-﻿using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Metadata;
 
 namespace WhereAmIWeak.Models
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         private static bool _created = false;
         
--- src/WhereAmIWeak/Models/ApplicationUser.cs
+++ src/WhereAmIWeak/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNet.Identity;
 
 namespace WhereAmIWeak.Models
 {
+	// Add profile data for application users by adding properties to the ApplicationUser class
 	public class ApplicationUser : IdentityUser
 	{
 
