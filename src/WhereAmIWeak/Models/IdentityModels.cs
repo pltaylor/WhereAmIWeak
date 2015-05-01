81af6fbef6d6e275d6f3d35dfb8246b258b42fde
Perry Taylor <pltaylor3@gmail.com>
2015-05-01 09:34:07 -0400

    Update project to new 2015 rc

--- src/WhereAmIWeak/Models/IdentityModels.cs
+++ src/WhereAmIWeak/Models/IdentityModels.cs
@@ -1,28 +1,32 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Metadata;
+using Microsoft.Framework.OptionsModel;
 
 namespace WhereAmIWeak.Models
 {
-	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
     {
-        private static bool _created = false;
-        
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private static bool _created;
+
         public ApplicationDbContext()
-        {            
+        {
             // Create the database and schema if it doesn't exist
-            // This is a temporary workaround to create database until Entity Framework database migrations 
-            // are supported in ASP.NET 5
             if (!_created)
             {
-                Database.AsMigrationsEnabled().ApplyMigrations();
+                Database.AsRelational().ApplyMigrations();
                 _created = true;
             }
-        }
-        
-        protected override void OnConfiguring(DbContextOptions options)
-        {
-            options.UseSqlServer();
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
